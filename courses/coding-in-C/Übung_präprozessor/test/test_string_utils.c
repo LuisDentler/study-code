a94e370c536d25f9bc9e13c4a2beac4432158d47
LuisDentler <luis.dentler06@gmail.com>
2026-02-26 18:20:53 +0000

    First test case

--- courses/coding-in-C/Übung_präprozessor/test/test_string_utils.c
+++ courses/coding-in-C/Übung_präprozessor/test/test_string_utils.c
@@ -1,11 +1,11 @@
 #include <assert.h>
 #include "../include/string_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
     /*test for costume_len*/
-    assert(custom_len("Schule") == 6);
-
-
-    printf("Tests erfolgreich.");
+    assert(custom_len("Schule") == 5);
+    printf("Tests erfolgreich.\n");
     return 0;
 }
