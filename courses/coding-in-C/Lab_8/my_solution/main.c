e35d46a4293f9275941f4c0f15fde1d3058016f9
LuisDentler <luis.dentler06@gmail.com>
2026-03-03 17:28:35 +0000

    Add main.c

---
+++ courses/coding-in-C/Lab_8/my_solution/main.c
@@ -0,0 +1,12 @@
+#include "include/playlist.h"
+
+int main(void)
+{
+    printf("Max Playlistgröße ist auf %d gesetzt.\n",MAX_SONGS);
+    Playlist myPlaylist;
+    init_playlist(&myPlaylist);
+
+    add_song(&myPlaylist, "Crawling", "Linkin Park");
+    add_song(&myPlaylist, "Layla", "Eric Clapton");
+    add_song(&myPlaylist, "Esperanto", "Max Herre");
+}
