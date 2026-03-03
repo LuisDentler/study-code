167de629afa6c6b75c8e1f7e03f6a8dc77718a94
LuisDentler <luis.dentler06@gmail.com>
2026-03-03 17:30:43 +0000

    Updated main.c

--- courses/coding-in-C/Lab_8/my_solution/main.c
+++ courses/coding-in-C/Lab_8/my_solution/main.c
@@ -9,4 +9,14 @@ int main(void)
     add_song(&myPlaylist, "Crawling", "Linkin Park");
     add_song(&myPlaylist, "Layla", "Eric Clapton");
     add_song(&myPlaylist, "Esperanto", "Max Herre");
+
+    print_playlist(&myPlaylist);
+
+    delete_firstSong(&myPlaylist);
+
+    print_playlist(&myPlaylist);
+
+    delete_playlist(&myPlaylist);
+
+    return 0;
 }
