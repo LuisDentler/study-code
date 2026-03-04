8f1264daebe6c04a81b7ea62519bff84be8b6522
LuisDentler <luis.dentler06@gmail.com>
2026-03-04 14:53:22 +0000

    Add functionheader for sort playlist

--- courses/coding-in-C/Lab_9/my_solution/include/playlist.h
+++ courses/coding-in-C/Lab_9/my_solution/include/playlist.h
@@ -31,6 +31,6 @@ void delete_playlist(Playlist *playlist);
 
 Song* find_song_by_title(Playlist *playlist, const char *title);
 int count_songs_recursive(const Song *current);
-
+void sort_playlist_by_title(Playlist *playlist);
 
 #endif
